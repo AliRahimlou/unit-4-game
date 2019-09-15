10bcf55b5fda7ff7e6c82863fc2d7b055fcc548a
Ali Rahimlou <Rahimlou91@gmail.com>
2019-09-15 02:05:01 -0400

    code almost working

---
+++ assets/javascript/game.js
@@ -0,0 +1,81 @@
+
+ $( document ).ready(function() {
+    console.log( "ready!" );
+
+  var targetNumber = Math.floor(Math.random() * (102) +19);
+  var gem1 = Math.floor(Math.random() * (12) + 1);
+  var gem2 = Math.floor(Math.random() * (12) + 1);
+  var gem3 = Math.floor(Math.random() * (12) + 1);
+  var gem4 = Math.floor(Math.random() * (12) + 1);
+  var numberOptions = [gem1, gem2, gem3, gem4];
+  var counter = 0;
+  var wins = 0;
+  var losses = 0;
+
+
+  $("#magicNumber").html(targetNumber)
+
+
+
+
+
+// $("#gem1").on("click", function(){
+//     alert(gem1)
+// })
+
+// $("#gem2").on("click", function(){
+//     alert(gem2)
+// })
+
+// $("#gem3").on("click", function(){
+//     alert(gem3)
+// })
+
+// $("#gem4").on("click", function(){
+//     alert(gem4)
+// })
+ 
+  
+  
+      $.each(numberOptions,function(i, value){
+        $("#gem"+(i+1)).on("click", function(){
+            counter+=value;
+            $("#yourScore").html(counter)
+
+            if (counter === targetNumber) {
+                alert("You win!");
+                wins++;
+                $("#wins").html(wins);
+                reset();
+
+              }
+            
+              else if (counter >= targetNumber) {
+                alert("You lose!!");
+                losses++;
+                $("#losses").html(losses);
+                reset();
+              }
+
+            // alert(value)
+            // console.log ($("#gem"+(i+1)))
+            
+        
+              })
+      })
+
+      function reset(){
+        gem1 = Math.floor(Math.random() * (12) + 1);
+        gem2 = Math.floor(Math.random() * (12) + 1);
+        gem3 = Math.floor(Math.random() * (12) + 1);
+        gem4 = Math.floor(Math.random() * (12) + 1);
+        counter = 0;
+        targetNumber = Math.floor(Math.random() * (102) +19);
+        $("#magicNumber").html(targetNumber)
+        $("#yourScore").html(counter)
+      }
+
+    
+  
+  
+});
